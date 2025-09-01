b7ce40b25425810ddf27cafb764602758c9b0cfb
chermat mustapha <mchermat@outlook.fr>
2025-09-01 09:48:14 +0200

    debbug code for print factorial

---
+++ factorial.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
+
+if len(sys.argv) > 1:
+    f = factorial(int(sys.argv[1]))
+    print(f)
+else:
+    print("Usage: python3 script.py <nombre>")
